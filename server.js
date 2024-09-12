ea4f1e5bed3d7d4d457d7ebace3741adadba5b33
JanviN625 <janvinandwani@gmail.com>
2024-09-11 22:08:40 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const app = express();
 const port = 3000;
+const budget = require('./budget-data.json');
 
-app.use(cors());
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
-
+app.use('/', express.static('public'));
+//app.use(cors());
 
 app.get('/budget', (req, res) => {
     res.json(budget);
